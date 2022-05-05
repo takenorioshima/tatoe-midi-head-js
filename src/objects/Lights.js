334d00bdce8caae7113d513bfdc6403fe0c4137f
Takenori Oshima <takenorioshima@gmail.com>
2022-05-06 02:28:32 +0900

    update: Simplify light settings

--- src/objects/Lights.js
+++ src/objects/Lights.js
@@ -1,21 +1,16 @@
-import { Group, SpotLight, PointLight, AmbientLight, HemisphereLight, Color } from 'three';
+import { Group, AmbientLight, DirectionalLight, DirectionalLightHelper } from 'three';
 
 export default class BasicLights extends Group {
   constructor(...args) {
     super(...args);
 
-    const pointPink = new PointLight(0xFF6AFE, 0.7, 100, 1, 2);
-    const pointGreen = new PointLight(0x66FF81, 0.7, 100, 1, 2);
-    const dir = new SpotLight(0xFFFFFF, 0.8, 7, 0.8, 1, 1);
-    const ambi = new AmbientLight( 0x404040 , 2);
+    const dir = new DirectionalLight(0xCCCCCC);
+    const ambi = new AmbientLight(0xCCCCCC);
+    dir.position.set(1, 1, 1);
+    dir.target.position.set(0, 0, 0);
+
+    const lightHelper = new DirectionalLightHelper( dir );
     
-    dir.position.set(0, 1, 5);
-    dir.target.position.set(0,0,0);
-
-    pointPink.position.set(-2, -1, 2);
-    pointGreen.position.set(2, -1, 2);
-
-    this.add(ambi, pointPink, pointGreen);
-
+    this.add(ambi, dir, lightHelper);
   }
 }
